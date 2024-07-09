73e09812aa0f6891408d607bf1fee74d6e72466d
zxc-steve <63087711+zxc-steve@users.noreply.github.com>
2024-07-09 12:09:21 +0800

    Add files via upload

---
+++ SceneDelegate.swift
@@ -0,0 +1,14 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The app's scene delegate object.
+*/
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
